5817baa59c021248574e4429a8d2a2513a268c47
karantomar-26 <karantomar621@gmail.com>
2024-09-03 15:58:37 +0530

    first commit

---
+++ lib/actions/patient.actions.ts
@@ -0,0 +1,34 @@
+import { ID, Query } from "node-appwrite";
+
+import {
+  BUCKET_ID,
+  DATABASE_ID,
+  ENDPOINT,
+  PATIENT_COLLECTION_ID,
+  PROJECT_ID,
+  databases,
+  storage,
+  users,
+} from "../appwrite.config";
+
+export const createUser = async (user: CreateUserParams) => {
+  try {
+    const newuser = await users.create(
+      ID.unique(),
+      user.email,
+      user.phone,
+      undefined,
+      user.name
+    );
+  } catch (error: any) {
+    // Check existing user
+    if (error && error?.code === 409) {
+      const existingUser = await users.list([
+        Query.equal("email", [user.email]),
+      ]);
+
+      return existingUser.users[0];
+    }
+    console.error("An error occurred while creating a new user:", error);
+  }
+};
